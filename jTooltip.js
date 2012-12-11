6320a68e65825f7f7cbf6b74cc1188ec0aa41efd
rtancman <rtancman@gmail.com>
2012-12-11 01:54:30 -0200

    exemplo jTooltip

--- jTooltip.js
+++ jTooltip.js
@@ -8,9 +8,9 @@ var jTooltipOptions = {
   popElementArrow : null,
   scrollTop : null,
   popName : '',
-  popTopArrow : 30,
-  popTopArrowBottom : 105,
-  limitScrollTop : 345,
+  popTopArrow : 0,
+  popTopArrowBottom : 0,
+  limitScrollTop : 0,
   listElements : null
 }, target = {};
 
@@ -35,63 +35,71 @@ var jTooltip = function(options){
   this.init = function(element){
     var objTooltip = this;
     jQuery(element).live('mouseenter', function() {
-        var objElement = jQuery(this);
-        objTooltip.idElement = objElement.attr('data-tooltip-pop');
-        objTooltip.listElements = jQuery('.tooltip-pop-'+this.popName);
+        objTooltip.objElement = jQuery(this);
+        objTooltip.idElement = objTooltip.objElement.attr('data-tooltip-pop');
+        
         if(objTooltip.idElement){
           objTooltip.timer = setTimeout(function () {
             
-          objTooltip.topElement = parseInt(objElement.offset().top);
-          objTooltip.leftElement = parseInt(objElement.offset().left);
-          objTooltip.scrollTop = jQuery(window).scrollTop();
+            objTooltip.topElement = parseInt(objTooltip.objElement.offset().top);
+            objTooltip.leftElement = parseInt(objTooltip.objElement.offset().left);
+            objTooltip.scrollTop = jQuery(window).scrollTop();
 
-          objTooltip.popElement = jQuery('#tooltip-pop-'+objTooltip.popName+'-'+objTooltip.idElement);
-          objTooltip.popElementArrow = objTooltip.popElement.find('.extra-info-arrow');
-          objTooltip.popElement.css('left', objTooltip.leftElement + 'px');
-          objTooltip.popElement.fadeIn(300);
-          objTooltip.popElement.addClass('active');
-          objTooltip.popElement.removeClass('off');
+            objTooltip.popElement = jQuery('#tooltip-pop-'+objTooltip.popName+'-'+objTooltip.idElement);
+            objTooltip.popElementArrow = objTooltip.popElement.find('.arrow');
+            objTooltip.popElement.css('left', objTooltip.leftElement + 'px');
+            objTooltip.popElement.fadeIn(300);
+            objTooltip.popElement.addClass('active');
+            objTooltip.popElement.removeClass('off');
 
-          if( objTooltip.scrollTop >= objTooltip.limitScrollTop ){
-            objTooltip.popElementArrow.removeClass('bottom-arrow');
-            objTooltip.popElementArrow.addClass('top-arrow');
-            objTooltip.popElementArrow.attr('src','/img/top-arrow.png');
-            objTooltip.popElement.css('top', parseInt(objTooltip.topElement + objTooltip.popTopArrowBottom) + 'px');
-          }else{
-            objTooltip.popElementArrow.removeClass('top-arrow');
-            objTooltip.popElementArrow.addClass('bottom-arrow');
-            objTooltip.popElementArrow.attr('src','/img/bottom-arrow.png');
-            objTooltip.popElement.css('top', parseInt(objTooltip.topElement - objTooltip.popElement.height() - objTooltip.popTopArrow) + 'px');
-          }
+            if( objTooltip.scrollTop >= objTooltip.limitScrollTop ){
+              objTooltip.popElementArrow.removeClass('bottom');
+              objTooltip.popElementArrow.addClass('top');
+              objTooltip.popElement.css('top', parseInt(objTooltip.topElement + objTooltip.popTopArrowBottom) + 'px');
+            }else{
+              objTooltip.popElementArrow.removeClass('top');
+              objTooltip.popElementArrow.addClass('bottom');
+              objTooltip.popElement.css('top', parseInt(objTooltip.topElement - objTooltip.popElement.height() - objTooltip.popTopArrow) + 'px');
+            }
           
           }, 200);
       }
     }).live('mouseleave', function() {
       clearTimeout(objTooltip.timer);
-      var auxMouseenter = false;
+      var auxMouseenter = false,
+      elementIframe = false;
       if(objTooltip.popElement){
-        objTooltip.popElement.live('mouseenter mouseleave', function(event) {
-          if(event.type == 'mouseleave'){
-            objTooltip.clearListColaborators();
-          }else{
+        elementIframe = objTooltip.popElement.find('iframe');
+        if(elementIframe.length > 0 ){
             auxMouseenter = true;
-          }
-        });
+            jQuery('.tooltip-btn-close-'+objTooltip.popName).live('click', function() {
+              objTooltip.clear();
+            });  
+        }else{
+          objTooltip.popElement.live('mouseenter mouseleave', function(event) {
+            if(event.type == 'mouseleave'){
+              objTooltip.clear();
+            }else{
+              auxMouseenter = true;
+            }
+          });
+        }
       }
       setTimeout(function () {
         if(auxMouseenter == false){
-          objTooltip.clearListColaborators();
+          objTooltip.clear();
         }
-      }, 200);
+      }, 300);
     });
   };
 
-  this.clearListColaborators = function(){
-    objTooltip.listElements.fadeOut(300);
-    objTooltip.listElements.removeClass('active');
-    objTooltip.listElements.addClass('off');
-    objTooltip.listElements.css('left','0');
-    objTooltip.listElements.css('top','0');
+  this.clear = function(){
+    jQuery('.tooltip-pop-'+this.popName).fadeOut(200, function() {
+      jQuery(this).removeClass('active');
+      jQuery(this).addClass('off');
+      jQuery(this).css('left','0');
+      jQuery(this).css('top','0');
+    });
   }
 
 
